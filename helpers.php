558142dd78ddbf8763271d0892fd26913d96fb74
zakie khan <121384625+zakie2003@users.noreply.github.com>
2024-12-26 14:44:21 +0530

    first commit

---
+++ helpers.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @param string $path 
+ * @return string  
+ */
+
+ function base_path($path=''){
+    return __DIR__ . "/" . $path;
+ }
+
+?>
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+    require "../helpers.php";
+     require base_path("views/home.php");
+?>
